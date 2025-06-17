11d2a32e34bc9319c223ffa5257f9034620b775c
motty93 <rdwbocungelt5@gmail.com>
2025-06-18 00:20:33 +0900

    fix: mime type

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"mime"
 	"net/http"
+	"os"
 
 	"mhp-rooms/internal/handlers"
 
@@ -11,13 +13,27 @@ import (
 )
 
 func main() {
+	// MIMEタイプを設定
+	mime.AddExtensionType(".css", "text/css")
+	mime.AddExtensionType(".js", "application/javascript")
+	mime.AddExtensionType(".png", "image/png")
+	mime.AddExtensionType(".jpg", "image/jpeg")
+	mime.AddExtensionType(".ico", "image/x-icon")
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", handlers.HomeHandler).Methods("GET")
 	r.HandleFunc("/hello", handlers.HelloHandler).Methods("GET")
 
+	// 静的ファイルの配信
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
 
-	fmt.Println("サーバーを起動しています... http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	// ポート設定（Fly.ioの環境変数に対応）
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Printf("サーバーを起動しています... :%s\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
